059744a9bee0b82b9a66fabf3a02c17ce5f50c98
ifBars <114284668+ifBars@users.noreply.github.com>
2023-11-19 06:14:43 -0700

    Initial commit

---
+++ CrackMe.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <cstring> // Include the C string header
+#include <cstdlib> // Include the C standard library header
+#include <ctime>   // Include the C time header
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+using namespace std;
+
+int performMathOperation(int a, int b) {
+    return (a * 2) + (b / 2);
+}
+
+void dummyFunction() {
+    int result = performMathOperation(10, 20);
+    int result2 = performMathOperation(result, result * 2);
+}
+
+const char* generatePassword() {
+    static const char part1[] = "ilike";
+    static const char part2[] = "hotdogs";
+
+    // Concatenate the string literals
+    std::string concatenatedPassword = part1;
+    concatenatedPassword += part2;
+
+    // Dynamically allocate memory for the password
+    char* password = new char[concatenatedPassword.size() + 1];
+
+    // Copy the concatenated password to the dynamically allocated memory
+    strcpy_s(password, concatenatedPassword.size() + 1, concatenatedPassword.c_str());
+
+    return password;
+}
+
+void obfuscatedSleep() {
+    Sleep(2000);
+}
+
+int main() {
+    const char* xYz = generatePassword();
+    char aBc[256];
+
+    while (true) {
+        std::cout << "Enter the password: ";
+        std::cin >> aBc;
+
+        if (std::strcmp(aBc, xYz) == 0) {
+            std::cout << "Congratulations! You've cracked the password.\n";
+
+            dummyFunction(); // Include dummy function calls
+
+            obfuscatedSleep(); // Use an obfuscated sleep function
+
+            break;
+        }
+        else {
+            std::cout << "Incorrect password. Try again.\n";
+        }
+    }
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple-CPP-CrackMe
+ A simple CrackMe made with C++ for beginners looking to learn reverse engineering/decomipiling
